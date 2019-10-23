a518a36213f0b077b887bef169131e093370c885
natsunoyoru97 <honey970125@gmail.com>
2019-10-23 19:11:02 +0800

    更新第15天内容（有bug）

---
+++ vitality_2.1/src/fifo.c
@@ -0,0 +1,56 @@
+//the FIFO Library
+#include "bootpack.h"
+
+#define FLAGS_OVERRUN    0x0001
+
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf)
+//初始化FIFO
+{
+	fifo->size = size;
+	fifo->buf = buf;
+	fifo->free = size; //缓冲区大小
+	fifo->flags = 0;
+	fifo->p = 0; //写入位置
+	fifo->q = 0; //读取位置
+	return;
+}
+
+int fifo32_put(struct FIFO32 *fifo, int data)
+//向FIFO传递数据
+{
+	if (fifo->free == 0) {
+		//没有闲置空间
+		fifo->flags |= FLAGS_OVERRUN;
+		return -1;
+	}
+	fifo->buf[fifo->p] = data;
+	fifo->p++;
+	if (fifo->p == fifo->size) {
+		fifo->p = 0;
+	}
+	fifo->free--;
+	return 0;
+}
+
+int fifo32_get(struct FIFO32 *fifo)
+//从FIFO取出数据
+{
+	int data;
+	if (fifo->free == fifo->size) {
+		//缓冲区为空
+		return -1;
+	}
+	data = fifo->buf[fifo->q];
+	fifo->q++;
+	if (fifo->q == fifo->size) {
+		fifo->q = 0;
+	}
+	fifo->free++;
+	return data;
+}
+
+int fifo32_status(struct FIFO32 *fifo)
+//查看缓冲区状态
+{
+	return fifo->size - fifo->free;
+}
